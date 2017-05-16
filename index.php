2ea3a15d3db41f6928541634ad31a26cf3324bc0
astur4 <astur@MacBook-Pro-de-Roberto-2.local>
2017-05-16 17:56:59 +0200

    Completado 3 de 4 includes necesarios

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php include_once ("includes/head.php");?>
+</head>
+
+<body>
+
+    <?php include_once ("includes/nav.php");?>
+
+    <!-- .container -->
+    <div class="container">
+
+        <div class="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <?php include_once ("includes/scripts.php");?>
+
+</body>
+
+</html>
