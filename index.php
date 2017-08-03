49ee373eff49ab8abbe890437e8b849d9b2c4491
Roberto Moriyón <moriyon@gmail.com>
2017-08-03 13:06:30 +0200

    Several Updaets

--- index.php
+++ index.php
@@ -2,18 +2,7 @@
 <html lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <!--<link rel="icon" href="../../favicon.ico">-->
-
-    <title>Peña Olloniego F11</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="css/my.css" rel="stylesheet">
+    <?php include_once ("includes/head.php");?>
 </head>
 
 <body>
